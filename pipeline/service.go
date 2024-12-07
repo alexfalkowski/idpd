4cda469361d93654c10d2177fb077796f43560b8
Alex Falkowski <alexrfalkowski@gmail.com>
2024-12-07 19:54:46 +0100

    feat(api): declare an ID
    Have a specific ID for pipeline

--- pipeline/service.go
+++ pipeline/service.go
@@ -4,19 +4,24 @@ import (
 	"strconv"
 )
 
-// Service coordinates all the pipeline activities.
-type Service struct {
-	repo Repository
-}
+type (
+	// Service coordinates all the pipeline activities.
+	Service struct {
+		repo Repository
+	}
+
+	// ID for service.
+	ID uint64
+)
 
 // NewService for pipeline.
 func NewService(repo Repository) *Service {
 	return &Service{repo: repo}
 }
 
-func (s *Service) Get(id uint64) (*Pipeline, error) {
-	if id == 0 {
-		return nil, ErrInvalidID
+func (s *Service) Get(id ID) (*Pipeline, error) {
+	if err := id.Valid(); err != nil {
+		return nil, err
 	}
 
 	return s.repo.Get(id)
@@ -32,6 +37,17 @@ func (s *Service) Create(p *Pipeline) (*Pipeline, error) {
 }
 
 // ID from raw id.
-func (s *Service) ID(id string) (uint64, error) {
-	return strconv.ParseUint(id, 10, 64)
+func (s *Service) ID(id string) ID {
+	i, _ := strconv.ParseUint(id, 10, 64)
+
+	return ID(i)
+}
+
+// Valid or error.
+func (i ID) Valid() error {
+	if i == 0 {
+		return ErrInvalidID
+	}
+
+	return nil
 }
