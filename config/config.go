9d5ece383aac885a8cb00597c89b2ca61efe599e
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-12-10 23:51:54 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.329.0 (#31)
    https://github.com/alexfalkowski/go-service/pull/1064

--- config/config.go
+++ config/config.go
@@ -1,22 +1,23 @@
 package config
 
 import (
-	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/config"
 	"github.com/alexfalkowski/idpd/health"
 )
-
-// NewConfig for config.
-func NewConfig(i *cmd.InputConfig) (*Config, error) {
-	c := &Config{}
-
-	return c, i.Decode(c)
-}
 
 // Config for the service.
 type Config struct {
 	Health         *health.Config `yaml:"health,omitempty" json:"health,omitempty" toml:"health,omitempty"`
 	*config.Config `yaml:",inline" json:",inline" toml:",inline"`
+}
+
+// Valid or error.
+func (c Config) Valid() error {
+	if c.Config == nil {
+		return config.ErrInvalidConfig
+	}
+
+	return c.Config.Valid()
 }
 
 func decorateConfig(cfg *Config) *config.Config {
